f703377c88713d6c52a4ce92f95f2c38a0ee4356
Tawhidul Islam Akram <66179434+tawhidul-islam-akram@users.noreply.github.com>
2023-07-30 04:50:00 +0600

    Add files via upload

---
+++ Automatic_water_tank_refill.ino
@@ -0,0 +1,68 @@
+int trigpin=A5;
+int echopin=A4;
+int distance;
+float duration;
+
+void setup()
+{
+	// initialize the LCD
+  Serial.begin(9600);
+
+  pinMode(trigpin,OUTPUT);
+  pinMode(echopin,INPUT);
+  pinMode(5, OUTPUT);  // use on-board LED
+
+   delay(400);
+
+
+}
+
+void loop()
+{
+ digitalWrite(trigpin,LOW);
+ delay(2);
+ digitalWrite(trigpin,HIGH);
+ delayMicroseconds(10);
+ digitalWrite(trigpin,LOW);
+
+ duration= pulseIn(echopin,HIGH);
+
+ distance = duration *0.034;
+
+ Serial.println(distance);
+
+ if(distance >=10)
+  {
+    digitalWrite(5,0);///pump on 
+
+  }
+  /*
+
+   else if(distance >=3 && distance<=7)
+  {
+    digitalWrite(5,0);///pump on 
+
+  }
+
+  else if(distance > 7 && distance<=9 ) 
+  {
+    digitalWrite(5,0); ///[pump off]
+
+  }
+*/
+
+  else if(distance < 10 ) 
+  {
+    digitalWrite(5,1); ///[pump off]
+
+  }
+
+
+ delay(200);
+}
+
+
+
+
+
+
